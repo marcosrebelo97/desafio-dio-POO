0b3b1666c42faa5a42c2fa68e5e688063e7c3fb6
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-10 11:26:43 -0400

    implementando metodos

--- desafio-dio-POO/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,12 +11,23 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progedir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosConcluidos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matricula em nenhum conteúdo!");
+        }
     }
 
-    public void calcularXp() {
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
+
     }
 
     public String getNome() {
